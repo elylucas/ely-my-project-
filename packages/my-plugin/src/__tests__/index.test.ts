439200945ff0cfc2c6c4984775f01d9e8de1456b
Ely Lucas <ely@meta-tek.net>
2021-03-15 16:53:40 -0600

    chore(my-plugin): fixed test

--- packages/my-plugin/src/__tests__/index.test.ts
+++ packages/my-plugin/src/__tests__/index.test.ts
@@ -2,9 +2,9 @@ import { sayHelloExtended } from '../index';
 
 describe('@ely-my-project/core', () => {
   describe('sayHelloExtended', () => {
-    it('should return back hello and name extended', () => {
+    it('should return back hello and name extended person', () => {
       const result = sayHelloExtended('Ely');
-      expect(result).toEqual('Hello Ely extended');
+      expect(result).toEqual('Hello Ely extended person');
     });
   });
 });
