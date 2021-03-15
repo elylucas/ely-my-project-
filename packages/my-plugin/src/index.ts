63f58915bf6da033ffa317e5166a1f5d1af86273
Ely Lucas <ely@meta-tek.net>
2021-03-15 16:52:50 -0600

    feat(my-plugin): making it friendlier

--- packages/my-plugin/src/index.ts
+++ packages/my-plugin/src/index.ts
@@ -1,5 +1,5 @@
 import { sayHello } from '@ely-my-project/core';
 
 export const sayHelloExtended = (name: string): string => {
-  return sayHello(name) + ' extended';
+  return sayHello(name) + ' extended person!';
 };
