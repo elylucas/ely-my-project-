9edf79f8479d50f4144152f88f5692d76e9e5c84
Ely Lucas <ely@meta-tek.net>
2021-03-18 08:26:00 -0600

    feat(core): 7 Ds?!

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -9,5 +9,5 @@ export const sayHello = (name: string): string => {
 
 
 export const sayHello2 = (name: string, age: number): string => {
-  return 'Hello ' + name + ' 2aaaaaaaaadddddddda You are ' + age;
+  return 'Hello ' + name + ' 2aaaaaaaaaddddddddda You are ' + age;
 };
